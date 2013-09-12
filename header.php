6fe94a9c10cf782488a1d70a78fb886ee63c663b
urielaero <aero.uriel@gmail.com>
2013-09-12 15:54:59 -0500

    ruteo url del menu

--- header.php
+++ header.php
@@ -15,23 +15,24 @@
 		<div id='header'>
 			
 <div class='menu blog'><div class='container'>
-	<a href='/' class='logo'><img src='http://comparatuescuela/templates/mtev1/img/home/logo.png' alt='home/logo.png'  /></a>
-	<a href='/compara/'>CONOCE
+<?php $url = 'http://comparatuescuela.proyects.spaceshiplabs.com'?>
+	<a href='/' class='logo'><img src='<?php bloginfo( 'template_directory' ); ?>/img/home/logo.png' alt='home/logo.png'  /></a>
+	<a href='<?=$url?>/compara/'>CONOCE
 		<span class='icon sprites'></span>
 		<span class='circle'></span>
 		<span class='decor'>1</span>
 	</a>
-	<a href='/compara/escuelas/'>COMPARA
+	<a href='<?=$url?>/compara/escuelas/'>COMPARA
 		<span class='icon sprites'></span>
 		<span class='circle'></span>
 		<span class='decor'>2</span>
 	</a>
-	<a href='/califica-tu-escuela/califica/'>CALIFICA
+	<a href='<?=$url?>/califica-tu-escuela/califica/'>CALIFICA
 		<span class='icon sprites'></span>
 		<span class='circle'></span>
 		<span class='decor'>3</span>
 	</a>
-	<a href='/mejora'>MEJORA
+	<a href='<?=$url?>/mejora'>MEJORA
 		<span class='icon sprites'></span>
 		<span class='circle'></span>
 		<span class='decor'>4</span>
@@ -51,8 +52,8 @@
 			<input type='submit' value='' />
 			<div class='clear'></div>
 		</form>-->
-		<a href='/quienes-somos'>¿Quiénes somos?</a>
-		<a href='/preguntas-frecuentes'>Preguntas frecuentes</a>
+		<a href='<?=$url?>/quienes-somos'>¿Quiénes somos?</a>
+		<a href='<?=$url?>/preguntas-frecuentes'>Preguntas frecuentes</a>
 
 	</div>
 
@@ -63,7 +64,7 @@
 	<ul>
 		<li>
 			<a href="/">
-				<img src='http://comparatuescuela/templates/mtev1/img/breadcrumb/home.png' alt='breadcrumb/home.png'  />			</a>
+				<img src='<?php bloginfo( 'template_directory' ); ?>/img/breadcrumb/home.png' alt='breadcrumb/home.png'  />			</a>
 		</li>
 
 				<li>
@@ -73,7 +74,7 @@
 				
 	</ul>
 </div>
-<div class='perfil container'><form action='/compara#resultados' method='get' accept-charset='utf-8' class='general-search' id='general-search'>
+<div class='perfil container'><form action='<?=$url?>/compara#resultados' method='get' accept-charset='utf-8' class='general-search' id='general-search'>
 	<p class='button-frame'>
 		<input name='term' id='name-input' type='text' placeholder='Busca tu escuela' value='' />
 		<input type='submit' class='integrated' value='' />
@@ -90,7 +91,7 @@
 	<hr />
 	<hr />
 	<hr />	
-	<img src='http://comparatuescuela/templates/mtev1/img/home/palomita.png' alt='home/palomita.png'  />	<img src='http://comparatuescuela/templates/mtev1/img/home/birrete_small.png' alt='home/birrete_small.png'  /></div>
+	<img src='<?php bloginfo( 'template_directory' ); ?>/img/home/palomita.png' alt='home/palomita.png'  />	<img src='<?php bloginfo( 'template_directory' ); ?>/img/home/birrete_small.png' alt='home/birrete_small.png'  /></div>
 
 </div>
 <div class="decorations perfil">

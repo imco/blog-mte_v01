833cf3bc9980f0aa98c73995fd2225fbdd80ae07
urielaero <aero.uriel@gmail.com>
2013-10-01 20:00:56 -0500

    img sigue muy grande...

--- header.php
+++ header.php
@@ -30,8 +30,8 @@
 	echo "<meta property='og:image' content='{$url_logo}' />";
 	?>
 	<meta property="og:image:type" content="image/png">
-	<meta property="og:image:width" content="230">
-	<meta property="og:image:height" content="210">
+	<meta property="og:image:width" content="300">
+	<meta property="og:image:height" content="300">
 	
  </head>
  <body>
